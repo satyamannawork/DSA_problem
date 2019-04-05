4789dd476672e5156492ee642bc4020a3a40abf3
satyajit <satyajit@satyajit-Lenovo-ideapad-320-15ISK>
2019-04-05 08:41:41 +0530

    from home :  05/04/2019

---
+++ ArrayProblems/src/com/satya/geekandgeeks/DiveandConq.java
@@ -0,0 +1,31 @@
+package com.satya.geekandgeeks;
+
+public class DiveandConq {
+	 static int sum  =0 ;
+	
+	
+	 public static void main(String argc[]) {
+		 int[] arr = {0, 1, 2, 3, 4, 5};
+		 System.out.println(DiveandConq.add(arr, 0, arr.length-1));
+		 //DiveandConq.add(arr, 0, arr.length-1);
+	     	 
+		 
+		 
+	 }
+	 
+	static  int  add(int[]  arr,int s,int e) {
+		
+		 if(s<e) {
+			 int  mid = (s+e)/2 ;
+			 add(arr, s,mid);
+			 add(arr,mid+1 ,e);
+		 }
+		 else {
+			sum=sum+ arr[s];
+		 }
+		 return sum ;
+		 
+	 }
+	 
+	 
+}
